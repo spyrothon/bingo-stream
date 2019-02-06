b7e1d82d42122e8e872bc21e1cc7699fbdc723b2
Jon <jonegeland@gmail.com>
2019-02-05 21:13:23 -0500

    Nameplate theming, gradients, and shadows.

--- elements/atoms/bl-nameplate.js
+++ elements/atoms/bl-nameplate.js
@@ -14,9 +14,36 @@ class BLNameplate extends LitElement {
           display: block;
           color: var(--bl-color-secondary);
         }
+
+        .nameplate {
+          /* Sizing */
+          position: relative;
+          width: 320px;
+          padding: 0.50em;
+          margin: 4px;
+          text-align: center;
+
+          /* Border gradient */
+          border-width: 1px;
+          border-style: solid;
+          border-image: var(--bl-gradient-secondary);
+          border-image-slice: 1;
+
+          /* Glow */
+          box-shadow: var(--bl-inset-shadow-secondary);
+
+          /* Text appearance */
+          text-transform: uppercase;
+          font-size: 22px;
+          font-weight: 300;
+          letter-spacing: 0.13em;
+          text-shadow: var(--bl-shadow-secondary);
+        }
       </style>
 
-      <p>Runner: ${this.name}</p>
+      <div class="nameplate">
+        ${this.name}
+      </div>
     `;
   }
 }
